126b8b6ef3ff15dea260c06cf6f8d543dd4f1ed2
kundanx <siwakotik89@gmail.com>
2023-12-01 11:18:56 +0545

    made minor changes

--- src/dev_pi_communicate/dev_pi_communicate/publisher.py
+++ src/dev_pi_communicate/dev_pi_communicate/publisher.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 
 import rclpy 
-import time
 from rclpy.node import Node 
 from std_msgs.msg import String
 
@@ -9,26 +8,26 @@ from std_msgs.msg import String
 
 class publisher(Node):
 
-    counter =0
-    vel_cmd = String()
-    recieve_data_cml = String()
+  
     def __init__(self):
         
         super().__init__("publisher_node")
-        self.recieve_data = self.create_subscription(String, "recieve_data", self.recieve_cml_callback, 10)
+
+        self.counter =0
+        self.vel_cmd = String()
+        self.recieve_data_cml = String()
+
         self.cmd_pub = self.create_publisher(String,"/string_topic", 10)
         self.create_timer(0.5, self.send_velocity_command)
         self.get_logger().info("Publishing command")
-    
-    def recieve_cml_callback(self, recieve_data_cml:String ):
-        self.get_logger().info(self.recieve_data_cml)
-        self.vel_cmd = self.recieve_data_cml
+
         
     
     def send_velocity_command(self):
-        # self.vel_cmd.data = "publishing data" + str(self.counter)
-        self.cmd_pub.publish(self.vel_cmd)
-        self.counter= self.counter +1
+        self.vel_cmd.data =  "publishing data" + str(self.counter) 
+        self.cmd_pub.publish( self.vel_cmd)
+        self.get_logger().info(str(self.vel_cmd))
+        self.counter= self.counter + 1 
 
 def main(args=None):
     rclpy.init(args=args)
