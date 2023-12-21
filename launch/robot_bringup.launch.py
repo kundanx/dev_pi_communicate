a0d173ee5cefb3b4563f0684a9ca05411c6c6e35
kundanx <siwakotik89@gmail.com>
2023-12-21 13:24:40 +0545

    Improved robot_bringup.launch.py

--- launch/robot_bringup.launch.py
+++ launch/robot_bringup.launch.py
@@ -12,9 +12,20 @@ from launch.launch_description_sources import PythonLaunchDescriptionSource
 import os
 
 def generate_launch_description():
-    pkg_path = os.path.join(get_package_share_directory('dev_pi_communicate'))
 
- 
+    # launch transforms
+    tf_path = os.path.join(get_package_share_directory('dev_pi_communicate'),'launch','transform.launch.py')
+    tf=IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([tf_path])
+    )
+
+    # joy_node from joy package to interface joystick_contr
+    joystick_controller=Node(
+        package='joy',
+        executable='joy_node',
+        output='screen'
+    )
+
     # serial comms to read from STM32 
     serial_comms_node= Node(
         package='dev_pi_communicate',
@@ -45,7 +56,10 @@ def generate_launch_description():
 
     # run the node
     return LaunchDescription([
+        joystick_controller,
         serial_comms_node,
-        camera_node,
-        joy_node
+        # camera_node,
+        # teleop_key_node,
+        joy_node,
+        tf
     ])
