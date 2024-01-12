b275ba659288b212c69be79db6c77b3d69836012
rpi2 <078bei018.kundan@pcampus.edu.np>
2024-01-12 22:27:19 +0545

    interfaced ds4_driver for better communication with ps4 controller

--- launch/robot_bringup.launch.py
+++ launch/robot_bringup.launch.py
@@ -7,6 +7,7 @@ from launch.actions import IncludeLaunchDescription, DeclareLaunchArgument
 from launch_ros.actions import Node
 from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
 from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch_xml.launch_description_sources import XMLLaunchDescriptionSource
 
 
 import os
@@ -14,16 +15,28 @@ import os
 def generate_launch_description():
 
     # launch transforms
-    tf_path = os.path.join(get_package_share_directory('dev_pi_communicate'),'launch','transform.launch.py')
+    tf_path = os.path.join(get_package_share_directory('dev_pi_communicate'),'launch/transform.launch.py')
     tf=IncludeLaunchDescription(
         PythonLaunchDescriptionSource([tf_path])
     )
 
     # joy_node from joy package to interface joystick_contr
+    param_file= os.path.join(get_package_share_directory('dev_pi_communicate'),'parameter_files','joy-params.yaml')
     joy_node=Node(
         package='joy',
         executable='joy_node',
         output='screen'
+        # parameters=[ "deadzone: 0.5",
+        #     "autorepeat_rate: 50",
+        #     "sticky_buttons: false",
+        #     "coalesce_interval_ms: 20"
+        #     ]
+    )
+
+    # ds4_node from ds4_driver package to interface joystick contro
+    ds4_path = os.path.join(get_package_share_directory('ds4_driver'),'launch/ds4_driver.launch.xml')
+    ds4_driver=IncludeLaunchDescription(
+        XMLLaunchDescriptionSource([ds4_path],)
     )
 
 
@@ -33,6 +46,8 @@ def generate_launch_description():
         executable='serial_comm_node',
         output='screen'
     )
+
+
 
     # control robot using PS4
     ps4_node= Node(
@@ -48,11 +63,22 @@ def generate_launch_description():
         output='screen'
     )
 
+    micro_ros = Node(
+        package= 'micro_ros_agent',
+        executable='micro_ros_agent',
+        output= 'screen',
+        parameters=[
+            {'transport':'serial'},
+            {'dev':'/dev/serial/by-id/usb-Silicon_Labs_CP2102_USB_to_UART_Bridge_Controller_0001-if00-port0'}     
+        ]
+    )
     # run the node
     return LaunchDescription([
-        joy_node,
+        # joy_node,
+        # ds4_driver,
+        # micro_ros,
         ps4_node,
-        tf
-        # serial_comms_node
+        tf,
+        serial_comms_node
         # camera_node,
     ])
