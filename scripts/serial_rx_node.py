db6ac7932ee6a95ae030eb41a6659e2daf0ddd4d
rpi2 <078bei018.kundan@pcampus.edu.np>
2024-01-26 11:10:08 +0545

    Separate node to RX and TX data serially

---
+++ scripts/serial_rx_node.py
@@ -0,0 +1,142 @@
+#! /usr/bin/env python3
+
+# This node reads the data from usb serial port and publish the data to ekf filter package through
+# freewheel/odom topic
+
+import rclpy
+import struct
+import math
+import time
+
+
+from math import sin, cos, radians
+from rclpy.node import Node 
+from nav_msgs.msg import Odometry
+
+from dev_pi_communicate.crc8 import crc8
+from dev_pi_communicate.serial_comm import serial_comms
+
+START_BYTE= 0b10100101
+serial_baudrate = 115200
+serial_port_address_FTDI='/dev/serial/by-id/usb-FTDI_FT232R_USB_UART_A50285BI-if00-port0'
+serial_port_address_black='/dev/serial/by-id/usb-1a86_USB2.0-Serial-if00-port0'
+
+class Serial_comms_RX_node(Node):
+    
+    def __init__(self):
+
+        super().__init__("serial_comm_RX_node")
+        self.usb_port1 = serial_comms(serial_port_address_black, serial_baudrate)
+        
+        #  Odom data publisher
+        self.odom_publisher_ = self.create_publisher(Odometry, 'freewheel/odom', 10)
+        self.create_timer(0.05, self.serial_read_callback)
+
+        self.last_sent_time = time.time()
+        self.odom_seq = 0
+        self.get_logger().info("Recieving ready...")
+        
+    # Read callback function
+    def serial_read_callback(self):
+            
+            _data = self.usb_port1.read_data()
+            if (time.time() - self.last_sent_time > 0.05):
+                # data = [x, y, theta, vx, vy, omega]
+                data = struct.unpack("ffffff", _data[0:24])
+                odom_msg = Odometry()
+                odom_msg.header.stamp = self.get_clock().now().to_msg()
+                odom_msg.header.frame_id = 'odom'
+                odom_msg.child_frame_id = 'base_link'
+                odom_msg.pose.pose.position.x = data[0]
+                odom_msg.pose.pose.position.y = data[1]
+                odom_msg.pose.pose.position.z = 0.0
+                qw, qx, qy, qz = self.rollpitchyaw_to_quaternion(0.0, 0.0, data[2])
+                odom_msg.pose.pose.orientation.w = qw
+                odom_msg.pose.pose.orientation.x = qx
+                odom_msg.pose.pose.orientation.y = qy
+                odom_msg.pose.pose.orientation.z = qz
+                odom_msg.pose.covariance = [0.01, 0.0, 0.0, 0.0, 0.0, 0.0,
+                                            0.0, 0.01, 0.0, 0.0, 0.0, 0.0,
+                                            0.0, 0.0, 1.0, 0.0, 0.0, 0.0,
+                                            0.0, 0.0, 0.0, 1.0, 0.0, 0.0,
+                                            0.0, 0.0, 0.0, 0.0, 1.0, 0.0,
+                                            0.0, 0.0, 0.0, 0.0, 0.0, 0.030461]
+                odom_msg.twist.twist.linear.x = data[3]
+                odom_msg.twist.twist.linear.y = data[4]
+                odom_msg.twist.twist.linear.z = 0.0
+                odom_msg.twist.twist.angular.x = 0.0
+                odom_msg.twist.twist.angular.y = 0.0
+                odom_msg.twist.twist.angular.z = data[5]
+                odom_msg.twist.covariance = [0.25, 0.0, 0.0, 0.0, 0.0, 0.0,
+                                                0.0, 0.25, 0.0, 0.0, 0.0, 0.0,
+                                                0.0, 0.0, 1.0, 0.0, 0.0, 0.0,
+                                                0.0, 0.0, 0.0, 1.0, 0.0, 0.0,
+                                                0.0, 0.0, 0.0, 0.0, 1.0, 0.0,
+                                                0.0, 0.0, 0.0, 0.0, 0.0, 0.04]
+                self.odom_publisher_.publish(odom_msg)
+                self.odom_seq += 1
+                self.last_sent_time = time.time()
+                # self.get_logger().info('"%f %f %f %f %f %f"'
+                #                       %(data[0], data[1], data[2]*180/math.pi, data[3], data[4], data[5]))
+                
+        
+
+    def calculate_checksum(self , data = []):
+        digest = int()
+        for i in range(1,len(data)):
+            digest += data[i]
+        return int(digest)
+    
+    def calculate_crc(self, data=[]):
+        hash_func=crc8()
+        hash_func.update(data[1:])
+        return hash_func.digest()[0]
+    
+    def rollpitchyaw_to_quaternion(self,roll, pitch, yaw):
+        
+
+        cy = cos(yaw * 0.5)
+        sy = sin(yaw * 0.5)
+        cp = cos(pitch * 0.5)
+        sp = sin(pitch * 0.5)
+        cr = cos(roll * 0.5)
+        sr = sin(roll * 0.5)
+
+        qw = cy * cp * cr + sy * sp * sr
+        qx = cy * cp * sr - sy * sp * cr
+        qy = sy * cp * sr + cy * sp * cr
+        qz = sy * cp * cr - cy * sp * sr
+
+        return qw, qx, qy, qz
+    
+    def quaternon_to_rollpitchyaw(self,q):
+
+        # roll (x-axis rotation)
+        sinr_cosp = 2 * (q[3] * q[0] + q[1] * q[2])
+        cosr_cosp = 1 - 2 * (q[0] * q[0] + q[1] * q[1])
+        roll = math.atan2(sinr_cosp, cosr_cosp)
+
+        # pitch (y-axis rotation)
+        sinp = math.sqrt(1 + 2 * (q[3] * q[1] - q[0] * q[2]))
+        cosp = math.sqrt(1 - 2 * (q[3] * q[1] - q[0] * q[2]))
+        pitch = 2 * math.atan2(sinp, cosp) -math.pi / 2 
+
+        #  yaw (z-axis rotation)
+        siny_cosp = 2 * (q[3] * q[2] + q[0] * q[1])
+        cosy_cosp = 1 - 2 * (q[1] * q[1] + q[2] * q[2])
+        yaw = math.atan2(siny_cosp, cosy_cosp)
+
+        return roll, pitch, yaw
+    
+
+def main(args=None):
+    rclpy.init()
+    node = Serial_comms_RX_node()
+    try:
+        rclpy.spin(node)
+    except KeyboardInterrupt:  
+        pass
+    rclpy.shutdown()
+   
+if __name__ =='__main':
+    main()
---
+++ scripts/serial_tx_node.py
@@ -0,0 +1,81 @@
+#! /usr/bin/env python3
+
+# This node subscribes to /cmd_robot_vel topic to recieve velocity commands in serial data packet form 
+# and write the data to usb port..
+
+import rclpy
+import struct
+import math
+
+
+from math import sin, cos, radians
+from rclpy.node import Node 
+from nav_msgs.msg import Odometry
+from std_msgs.msg import Float64MultiArray
+
+from dev_pi_communicate.crc8 import crc8
+from dev_pi_communicate.serial_comm import serial_comms
+
+START_BYTE= 0b10100101
+serial_baudrate = 115200
+serial_port_address_FTDI='/dev/serial/by-id/usb-FTDI_FT232R_USB_UART_A50285BI-if00-port0'
+serial_port_address_black='/dev/serial/by-id/usb-1a86_USB2.0-Serial-if00-port0'
+    
+class Serial_comms_TX_node(Node):
+    
+    def __init__(self):
+
+        super().__init__("serial_comm_TX_node")
+
+        self.usb_port2 = serial_comms(serial_port_address_FTDI, serial_baudrate)
+
+        # Subscriber to joystick data
+        self.sub_joy = self.create_subscription(
+            Float64MultiArray,
+            "/cmd_robot_vel", #/cmd_robot_vel
+            self.send_joy_data,
+            10 )
+        
+        self.get_logger().info("Transmission ready...")
+        
+    # Joystick read callback function
+    def send_joy_data(self,msg:Float64MultiArray):
+        # print("here recieve")
+        print(f"{msg.data[0]=}\n{msg.data[1]=}\n{msg.data[2]=}")
+        joy_data=[
+            bytes(struct.pack("B",START_BYTE)),
+            bytes(struct.pack("d",msg.data[0])),
+            bytes(struct.pack("d",msg.data[1])),
+            bytes(struct.pack("d",msg.data[2]))
+        ]
+        joy_data = b''.join(joy_data)
+        hash=self.calculate_crc(joy_data)
+        joy_data=[joy_data,
+            bytes(struct.pack('B', hash)) 
+        ]
+        joy_data=b''.join(joy_data)
+        self.usb_port2.write_data(joy_data)
+        
+    def calculate_checksum(self , data = []):
+        digest = int()
+        for i in range(1,len(data)):
+            digest += data[i]
+        return int(digest)
+    
+    def calculate_crc(self, data=[]):
+        hash_func=crc8()
+        hash_func.update(data[1:])
+        return hash_func.digest()[0]
+    
+
+def main(args=None):
+    rclpy.init()
+    node = Serial_comms_TX_node()
+    try:
+        rclpy.spin(node)
+    except KeyboardInterrupt:  
+        pass
+    rclpy.shutdown()
+   
+if __name__ =='__main':
+    main()
