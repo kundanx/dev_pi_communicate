1a75b1dc2738cc995e846d5a37c98f3a6eb676cd
rpi2 <078bei018.kundan@pcampus.edu.np>
2024-04-27 04:37:51 +0545

    Added imuLink

---
+++ transforms/baseLink_imuLink_tf.py
@@ -0,0 +1,78 @@
+import math
+import sys
+
+from geometry_msgs.msg import TransformStamped
+
+import numpy as np
+
+import rclpy
+from rclpy.node import Node
+
+from tf2_ros.static_transform_broadcaster import StaticTransformBroadcaster
+
+
+def quaternion_from_euler(ai, aj, ak):
+    ai /= 2.0
+    aj /= 2.0
+    ak /= 2.0
+    ci = math.cos(ai)
+    si = math.sin(ai)
+    cj = math.cos(aj)
+    sj = math.sin(aj)
+    ck = math.cos(ak)
+    sk = math.sin(ak)
+    cc = ci*ck
+    cs = ci*sk
+    sc = si*ck
+    ss = si*sk
+
+    q = np.empty((4, ))
+    q[0] = cj*sc - sj*cs
+    q[1] = cj*ss + sj*cc
+    q[2] = cj*cs - sj*sc
+    q[3] = cj*cc + sj*ss
+
+    return q
+
+
+class StaticFramePublisher(Node):
+  
+    def __init__(self, transformation):
+        super().__init__('baseLink_imu_tf2_broadcaster')
+
+        self.tf_static_broadcaster = StaticTransformBroadcaster(self)
+
+        # Publish static transforms once at startup
+        self.make_transforms(transformation)
+
+    def make_transforms(self, transformation):
+        t = TransformStamped()
+
+        t.header.stamp = self.get_clock().now().to_msg()
+        t.header.frame_id = 'base_link'
+        t.child_frame_id = 'imu_link'
+
+        t.transform.translation.x = -0.1
+        t.transform.translation.y = 0.0
+        t.transform.translation.z = 0.08
+        quat = quaternion_from_euler( 0.0 ,0.0, 0.0 ) # [180, 0, -41.57,-0.72553337]
+        t.transform.rotation.x = quat[0]
+        t.transform.rotation.y = quat[1]
+        t.transform.rotation.z = quat[2]
+        t.transform.rotation.w = quat[3]
+
+        self.tf_static_broadcaster.sendTransform(t)
+        # self.get_logger().info(str("Base_Link to laser_frame transform published."))
+
+
+
+def main():
+    logger = rclpy.logging.get_logger('logger')
+    rclpy.init()
+    node = StaticFramePublisher(sys.argv)
+    try:
+        rclpy.spin(node)
+    except KeyboardInterrupt:
+        pass
+
+    rclpy.shutdown()
